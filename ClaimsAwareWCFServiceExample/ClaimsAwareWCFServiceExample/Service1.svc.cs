ea06c6d9e0e942c02f8075d2f633e399fe08b059
Intesar Alam <alam.kir@gmail.com>
2019-01-31 19:30:24 +0530

    31012019

---
+++ ClaimsAwareWCFServiceExample/ClaimsAwareWCFServiceExample/Service1.svc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Claims;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ClaimsAwareWCFServiceExample
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+       public string ComputeResponse(string input)  
+    {  
+        // Get the caller's identity from ClaimsPrincipal.Current  
+        ClaimsPrincipal claimsPrincipal = OperationContext.Current.ClaimsPrincipal;  
+
+        // Start generating the output  
+        StringBuilder builder = new StringBuilder();  
+        builder.AppendLine("Computed by ClaimsAwareWebService");  
+        builder.AppendLine("Input received from client:" + input);  
+
+        if (claimsPrincipal != null)  
+        {  
+            // Display the claims from the caller. Do not use this code in a production application.  
+            ClaimsIdentity identity = claimsPrincipal.Identity as ClaimsIdentity;  
+            builder.AppendLine("Client Name:" + identity.Name);  
+            builder.AppendLine("IsAuthenticated:" + identity.IsAuthenticated);  
+            builder.AppendLine("The service received the following issued claims of the client:");  
+
+            // Iterate over the caller’s claims and append to the output  
+            foreach (Claim claim in claimsPrincipal.Claims)  
+            {  
+                builder.AppendLine("ClaimType :" + claim.Type + "   ClaimValue:" + claim.Value);  
+            }  
+        }  
+
+        return builder.ToString();  
+    } 
+    }
+}
---
+++ ClaimsAwareWCFServiceExample/Client/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Client.ServiceReference1;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the client for the service  
+            Service1Client  client = new Service1Client ();
+
+            Console.WriteLine("-------------WCF Client Application--------------\n");
+
+            while (!ShouldQuitApplication())
+            {
+                try
+                {
+                    Console.WriteLine(client.ComputeResponse("Hello World"));
+                }
+                catch (CommunicationException e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine(e.StackTrace);
+                    Exception ex = e.InnerException;
+                    while (ex != null)
+                    {
+                        Console.WriteLine("===========================");
+                        Console.WriteLine(ex.Message);
+                        Console.WriteLine(ex.StackTrace);
+                        ex = ex.InnerException;
+                    }
+                }
+                catch (TimeoutException)
+                {
+                    Console.WriteLine("Timed out...");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An unexpected exception occurred.");
+                    Console.WriteLine(e.StackTrace);
+                }
+            }
+
+            client.Close();
+
+            if (client != null)
+            {
+                client.Abort();
+            }
+        }
+        static bool ShouldQuitApplication()
+        {
+            Console.WriteLine("---------------------------------------------------------------------");
+            Console.WriteLine("Press Enter key to invoke service, any other key to quit application:");
+            Console.WriteLine("----------------------------------------------------------------------");
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            if (keyInfo.Key == ConsoleKey.Enter)
+                return false;
+            return true;
+        }
+    }
+}
